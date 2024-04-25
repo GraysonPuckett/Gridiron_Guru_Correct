dc351611967e38a555a1cd973a83661197e0cc2a
GraysonPuckett <grayson.puckett@bobcats.gcsu.edu>
2024-04-25 09:06:44 -0400

    updated code 4/24/24

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = create_app()
 
 # Set the secret key. Remember, in a real application, use a more secure method
 # Ideally, load this from an environment variable or a config file
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', '12345')
+app.secret_key = '12345'
 
 if __name__ == '__main__':
     app.run(debug=True)
