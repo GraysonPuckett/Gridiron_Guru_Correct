d853c458be4c6f0a6c6f5d472de538211b98f4b4
GraysonPuckett <grayson.puckett@bobcats.gcsu.edu>
2024-04-25 09:12:01 -0400

    updated code 4/24/24

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 import os
 from app import create_app
 
+# Set the secret key. Remember, in a real application, use a more secure method
+# Ideally, load this from an environment variable or a config file
+app.secret_key = '12345'
 
 # Initialize the Flask application using the application factory
 app = create_app()
 
-# Set the secret key. Remember, in a real application, use a more secure method
-# Ideally, load this from an environment variable or a config file
-app.secret_key = '12345'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
