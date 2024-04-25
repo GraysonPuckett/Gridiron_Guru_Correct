9627f6868cdb5fbc608d67f224b133355f9f7844
GraysonPuckett <grayson.puckett@bobcats.gcsu.edu>
2024-04-25 09:20:43 -0400

    updated code 4/24/24

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
 # app.py
-import os
-from app import create_app
-
-# Set the secret key. Remember, in a real application, use a more secure method
-# Ideally, load this from an environment variable or a config file
-app.secret_key = '12345'
-
-# Initialize the Flask application using the application factory
-app = create_app()
-
-
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,31 @@
 # app/__init__.py
-from flask import Flask, g
+from flask import Flask
 from flask_mysqldb import MySQL
 import os
-
+from .app_factory import create_app
 mysql = MySQL()  # Global MySQL extension instance
 
 
-def create_app():
-    app = Flask(__name__)
+app = create_app()
+app.secret_key = '12345'
 
-    # Application Configuration
-    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'bryanmarshall.com')
-    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'uo6wzuqzpup3g')
-    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'rrgx24vazvll')
-    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'dbptztdppxhiif')
 
-    mysql.init_app(app)
+# Application Configuration
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'bryanmarshall.com')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'uo6wzuqzpup3g')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'rrgx24vazvll')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'dbptztdppxhiif')
 
-    # Import Blueprints
-    from .blueprints.home import home_bp
-    from .blueprints.fantasy import fantasy_bp
-    from .blueprints.core import core_bp
-    from .blueprints.mock_draft import mock_draft_bp
-    # Register Blueprints
-    app.register_blueprint(home_bp, url_prefix='/')
-    app.register_blueprint(fantasy_bp, url_prefix='/fantasy')
-    app.register_blueprint(core_bp, url_prefix='/core')
-    app.register_blueprint(mock_draft_bp, url_prefix='/mock_draft')
+mysql.init_app(app)
 
-    return app
+# Import Blueprints
+from .blueprints.home import home_bp
+from .blueprints.fantasy import fantasy_bp
+from .blueprints.core import core_bp
+from .blueprints.mock_draft import mock_draft_bp
+# Register Blueprints
+app.register_blueprint(home_bp, url_prefix='/')
+app.register_blueprint(fantasy_bp, url_prefix='/fantasy')
+app.register_blueprint(core_bp, url_prefix='/core')
+app.register_blueprint(mock_draft_bp, url_prefix='/mock_draft')
+
